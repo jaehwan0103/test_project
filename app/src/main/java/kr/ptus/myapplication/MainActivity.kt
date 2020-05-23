82f8d01062f37efd711a781f52e46d828acfe792
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-05-24 00:13:44 +0900

    PTUS 로그인 화면 구축  - Kotlin 제어    =>로그인 버튼 클릭시 "로그인버튼누름" 텍스트 출력

--- app/src/main/java/kr/ptus/myapplication/MainActivity.kt
+++ app/src/main/java/kr/ptus/myapplication/MainActivity.kt
@@ -2,11 +2,22 @@ package kr.ptus.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickLogin.setOnClickListener{
+            Log.d("로그인","로그인버튼 누름")
+
+        }
+
     }
+
+
+
 }
